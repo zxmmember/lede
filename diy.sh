298941556860744c0390eebb2f8e0c69aea6a1d8
zxmmember <61856798+zxmmember@users.noreply.github.com>
2021-03-02 17:23:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,11 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_generate
 
 #拦截广告的app
 
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
+#git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
